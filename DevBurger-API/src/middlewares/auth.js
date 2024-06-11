a6dcb12baff9df59fb6a83457354d0ee975d5270
BrunoMSantos04 <marciliosantosbruno@gmail.com>
2024-06-11 11:37:45 -0300

    JWT configurado

---
+++ DevBurger-API/src/middlewares/auth.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken'
+import authConfig from '../config/auth'
+
+function authMiddleware(request, response, next){
+    const authToken = request.headers.autorization
+
+    if(!authToken){
+        return response.status(401).json({error: 'Token not provide'})
+    }
+
+    const token = authToken.split(' ').at(1);
+
+    try {
+     jwt.verify(token, authConfig.secret, (err, decoded) => {
+        if (err) {
+            throw new Error()
+        }
+
+
+        request.userId = decoded.id
+
+     })   
+    } catch (err){
+        return response.status(401).json({error: 'Token is invalid'})
+    }
+
+    return next()
+}
+
+export default authMiddleware
--- DevBurger-API/src/routes.js
+++ DevBurger-API/src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import multer from "multer"
 import multerConfig from './config/multer'
+import authMiddleware from "./middlewares/auth"
 
 import UserController from "./app/controllers/UserController"
 import SessionController from "./app/controllers/SessionController"
@@ -11,8 +12,9 @@ const routes = new Router()
 const upload = multer(multerConfig)
 
 routes.post('/users', UserController.store )
-routes.post('/sessions', SessionController.store )
-routes.post('/products',upload.single('file') ,ProductController.store )
+routes.post('/session', SessionController.store )
+routes.use(authMiddleware)
+routes.post('/products', upload.single('file') ,ProductController.store )
 routes.get('/products', ProductController.index )
 
 export default routes 
